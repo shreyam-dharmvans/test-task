c8bf98a769b50657dd75ebc18538a47a093fbc0e
shreyam-dharmvans <sdharmvans@gmail.com>
2024-11-08 00:38:43 +0530

    db schema and connection

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
+
+
+main()
+    .then(() => console.log('DB is connected'))
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URL);
+}
 
 const app = express();
 const port = 8080 || process.env.PORT;
---
+++ backend/model/userSchema.js
@@ -0,0 +1,15 @@
+import Schema from 'mongoose';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+    }
+})
+
+export default User = mongoose.model('User', userSchema);
